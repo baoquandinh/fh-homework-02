9eb0dcbc1abd2a0009c9bd45a7eeeea60dceda45
Baoquan Dinh <baoquanxdinh@gmail.com>
2019-01-28 18:31:41 +0000

    Created welcome page and configured database

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,19 @@
+class BooksController < ApplicationController
+    def index
+    end
+
+    def add
+    end
+
+    def update
+    end
+
+    def delete
+    end
+
+    def listall
+    end
+    
+    def search
+    end
+end
