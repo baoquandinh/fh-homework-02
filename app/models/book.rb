cde3108da4e08a4a875b58d1bc9e621d41b9abd2
Baoquan Dinh <baoquanxdinh@gmail.com>
2019-01-29 21:51:20 +0000

    Fix issue where modal wouldn't disappear after adding new book. Added all required field to modal as well

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,6 @@
 class Book < ApplicationRecord
     validates :title, presence: true, length: {maximum: 140, minimum: 1}
     validates :author, presence: true, length: {maximum: 140, minimum: 2}
+    validates :genre, presence: true, length: {maximum: 140, minimum: 1}
     validates :year, presence: true, length: {maximum: 4, minimum: 4}
 end
